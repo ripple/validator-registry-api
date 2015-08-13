4a3216e0ebe241de189ac4c1f26b194e1e8a612d
wilsonianb <bwilson@ripple.com>
2015-08-13 13:52:26 -0700

    [TASK] Remove obsolete ValidationReports and CorrelationScores tables

---
+++ migrations/20150807135723-drop-validation-reports.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('ValidationReports');
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('ValidationReports', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      date: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      validators: {
+        type: Sequelize.JSON,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  }
+};
