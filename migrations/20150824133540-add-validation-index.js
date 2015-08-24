4b6e0bfeaba56ba7be798c7324af1e8b8d126666
wilsonianb <bwilson@ripple.com>
2015-08-24 14:18:08 -0700

    [TASK] Add composite index to validation table DEC-572

---
+++ migrations/20150824133540-add-validation-index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.addIndex('Validations', [
+        'validation_public_key',
+        'ledger_hash',
+        'reporter_public_key'
+      ], {
+        indexName: 'ValidationIndex',
+        indicesType: 'UNIQUE'
+      });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.removeIndex('Validations', '')
+  }
+};
